4c1e5a3a6e8abd3571133267d15159e840fe1ea4
ca954bj <ca954bj@gmail.com>
2019-04-10 12:24:09 +1000

    190410

--- YieldLine.py
+++ YieldLine.py
@@ -51,6 +51,7 @@ class Line:
 		self.node1 = node1
 		self.node2 = node2
 		self.direction = [node2.x - node1.x, node2.y - node1.y, node2.z - node1.z]
+		print('Direction', self.direction)
 	def node1(self):
 		return self.node1
 	def node2(self):
@@ -69,6 +70,7 @@ class Plane:
 		self.NormalVector = Operation.CrossProduct(lineset[0].direction, lineset[1].direction).value
 		if self.NormalVector[0] == 0 and self.NormalVector[1] == 0 and self.NormalVector[2] == 0:
 			self.NormalVector = Operation.CrossProduct(lineset[0].direction, lineset[2].direction).value
+		print("NormalVector", self.NormalVector)
 	
 	def lineset(self):
 		return self.lineset
@@ -86,7 +88,7 @@ class YieldLines:
 				if obj in obj2.lineset:
 					planeset.append(obj2)
 			if len(planeset) > 2 or len(planeset) == 0:
-				print("The lines are not right!")
+				print("The lines are not right! Error %d" % (len(planeset)))
 			elif len(planeset) == 2:
 				v1 = planeset[0].NormalVector
 				v2 = planeset[1].NormalVector
@@ -100,6 +102,7 @@ class YieldLines:
 				Moment = dangle*linelength*mp
 				if Moment.evalf(subs={h:2}) < 0:
 					Moment = -Moment
+				print('Moment', Moment)
 				MomentSum += Moment
 			elif len(planeset) == 1:
 				v1 = planeset[0].NormalVector
@@ -114,12 +117,87 @@ class YieldLines:
 				Moment = dangle*linelength*mp
 				if Moment.evalf(subs={h:2}) < 0:
 					Moment = -Moment
+				print('Moment', Moment)
 				MomentSum += Moment
 		self.MomentSum = MomentSum
 	def MomentSum(self):
 		return self.MomentSum
 		
 h = Symbol('h')
+
+# For check
+'''A = Node(61.0, 49.0, h)
+B = Node(28.25, 16.33, h)
+C = Node(28.25, 0, h)
+D = Node(68.0, 25.0, 0)
+E = Node(68.0, 0, 0)
+F = Node(95.0, 0, 0)
+G = Node(95.0, 25.0, 0)
+H = Node(95.0, 49.0, h)
+
+print('BC')
+BC = Line(B, C)
+print('BD')
+BD = Line(B, D)
+print('DE')
+DE = Line(D, E)
+print('AB')
+AB = Line(A, B)
+print('AD')
+AD = Line(A, D)
+print('EF')
+EF = Line(E, F)
+print('DG')
+DG = Line(D, G)
+print('AH')
+AH = Line(A, H)
+
+print('ABD')
+ABD = Plane([AB, BD, AD])
+print('BCDE')
+BCDE = Plane([BC, BD, DE])
+print('ADGH')
+ADGH = Plane([AH, AD, DG])
+print('DEFG')
+DEFG = Plane([DE, DG])
+
+Moment = 8*YieldLines([BC, BD, DE, AB, AD, AH, DG], 2996.1, h).MomentSum
+Moment = Moment.evalf(subs={h:4.0})
+print("For test, the tensile capacity is %f" % (Moment/1000))'''
+
+# Problem C1
+'''A = Node(0, 0, 0)
+B = Node(35.0, 0, 0)
+C = Node(35.0, 35.0, 0)
+D = Node(0, 35.0, 0)
+E = Node(55.0, 55.0, h)
+F = Node(55.0, 95.0, h)
+G = Node(0, 95.0, 0)
+H = Node(95.0, 55.0, h)
+I = Node(55.0, 0, 0)
+
+BC = Line(B, C)
+CD = Line(C, D)
+CE = Line(C, E)
+CF = Line(C, F)
+CH = Line(C, H)
+BH = Line(B, H)
+DF = Line(D, F)
+EF = Line(E, F)
+EH = Line(E, H)
+FG = Line(F, G)
+BI = Line(B, I)
+
+CEF = Plane([CE, EF, CF])
+CDF = Plane([CD, DF, CF])
+CEH = Plane([CE, EH, CH])
+BCH = Plane([BC, CH, BH])
+DFG = Plane([DF, FG])
+BHI = Plane([BH, BI])
+
+Moment = 4*YieldLines([CD, BC, CE, CF, CH, DF, BH, EF, EH], 2996.1, h).MomentSum
+Moment = Moment.evalf(subs={h:4.0})
+print("For C1, the tensile capacity is %f" % (Moment/1000))'''
 
 # Problem C2 103.9kN
 '''A = Node(0, 35.0, 0)
@@ -151,7 +229,7 @@ Moment = Moment.evalf(subs={h:4.0})
 print("For C2, the tensile capacity is %f" % (Moment/1000))'''
 
 # Problem C3 Case1 141.3kN
-'''A = Node(0, 0, 0)
+A = Node(0, 0, 0)
 D = Node(35.0, 35.0, 0)
 E = Node(0, 35.0, 0)
 B = Node(35.0, 0, 0)
@@ -175,11 +253,11 @@ EDFG = Plane([DE, DG, FG])
 Moment = 4*YieldLines([DE, BD, DG, FG, CD, CG, BC], 2996.1, h).MomentSum
 print(Moment)
 Moment = Moment.evalf(subs={h:4.0})
-print("For C3, the tensile capacity is %f" % (Moment/1000))'''
+print("For C3, the tensile capacity is %f" % (Moment/1000))
 
 # Problem C3 Case2 163.6kN
 
-A = Node(0, 0, 0)
+'''A = Node(0, 0, 0)
 D = Node(35.0, 35.0, 0)
 E = Node(0, 35.0, 0)
 B = Node(35.0, 0, 0)
@@ -203,9 +281,67 @@ EDFG = Plane([DE, DG, FG])
 Moment = 4*YieldLines([DE, BD, DG, FG, BG, CG, BC], 2996.1, h).MomentSum
 print(Moment)
 Moment = Moment.evalf(subs={h:4.0})
-print("For C3, the tensile capacity is %f" % (Moment/1000))
+print("For C3, the tensile capacity is %f" % (Moment/1000))'''
 
+# Problem C5 242.9
+'''A = Node(0, 0, 0)
+B = Node(68.0, 0, 0)
+C = Node(92.5, 0, h)
+D = Node(0, 68.0, 0)
+E = Node(0, 92.5, h)
+F = Node(68.0, 25.0, 0)
+G = Node(25.0, 68.0, 0)
+H = Node(92.5, 110-92.5, h)
+I = Node(110-92.5, 92.5, h)
 
+BF = Line(B, F)
+FG = Line(F, G)
+DG = Line(D, G)
+CH = Line(C, H)
+EI = Line(E, I)
+FH = Line(F, H)
+GI = Line(G, I)
+HI = Line(H, I)
+
+BCFH = Plane([CH, FH, BF])
+FGHI = Plane([FG, FH, HI, GI])
+DEGI = Plane([DG, GI, EI])
+
+Moment = 4*YieldLines([BF, FH, CH, FG, HI, GI, DG, EI], 2996.1, h).MomentSum
+Moment = Moment.evalf(subs={h:4.0})
+print("For C5, the tensile capacity is %f" % (Moment/1000))'''
+
+# Problem C6 184.6
+'''A = Node(2.5/sqrt(2), 0, h)
+B = Node(68.0, 0, 0)
+C = Node(190-68.0, 0, 0)
+D = Node(190-2.5/sqrt(2), 0, h)
+E = Node(68.0, 25.0, 0)
+F = Node(190-68.0, 25.0, 0)
+G = Node(55.0, 55.0, h)
+H = Node(55.0+80, 55.0, h)
+
+AG = Line(A, G)
+AE = Line(A, E)
+BE = Line(B, E)
+EG = Line(E, G)
+EF = Line(E, F)
+GH = Line(G, H)
+CF = Line(C, F)
+FH = Line(F, H)
+DF = Line(D, F)
+DH = Line(D, H)
+
+AEG = Plane([AE, EG, AG])
+ABE = Plane([AE, BE])
+BCEF = Plane([BE, EF, CF])
+EFGH = Plane([EF, FH, GH, EG])
+CDF = Plane([CF, DF])
+DFH = Plane([DF, FH, DH])
+
+Moment = 4*YieldLines([AE, BE, EG, AG, EF, GH, CF, FH, DF, DH], 2996.1, h).MomentSum
+Moment = Moment.evalf(subs={h:4.0})
+print("For C6, the tensile capacity is %f" % (Moment/1000))'''
 				
 				
 
